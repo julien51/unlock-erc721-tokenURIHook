2a2df0e0dec5eee21e4e3783a2e191507dfd9ecc
Julien Genestoux <julien.genestoux@gmail.com>
2022-04-01 21:13:55 -0400

    removed output

--- scripts/generate.js
+++ scripts/generate.js
@@ -18,74 +18,79 @@ async function* getFiles(dir) {
   }
 }
 
-async function print(arr, cb) {
-  // Number of arrays
-  const n = arr.length;
-
-  // To keep track of next element in
-  // each of the n arrays
-  const indices = new Array(n);
-
-  // Initialize with first element's index
-  for (let i = 0; i < n; i++) indices[i] = 0;
-
-  while (true) {
-    // Print current combination
-    let combo = [];
-    for (let i = 0; i < n; i++) {
-      combo.push(arr[i][indices[i]]);
-    }
-    console.log('x')
-    await cb(combo);
-    combo = []; // reset
-
-    // Find the rightmost array that has more
-    // elements left after the current element
-    // in that array
-    let next = n - 1;
-    while (next >= 0 && indices[next] + 1 >= arr[next].length) next--;
-
-    // No such array is found so no more
-    // combinations left
-    if (next < 0) return;
-
-    // If found move to next element in that
-    // array
-    indices[next]++;
-
-    // For all arrays to the right of this
-    // array current index again points to
-    // first element
-    for (let i = next + 1; i < n; i++) indices[i] = 0;
-  }
-}
+// Then, we will randomly pick some values and build outputs!
 
 async function main() {
   // Import all SVG layers
-  const svgs = {};
+  const svg = {};
+
   for await (const f of getFiles("scripts/svg/")) {
     const fullPath = f.split("/");
-    const type = fullPath[fullPath.length - 2];
-    if (!svgs[type]) {
-      svgs[type] = [];
+    const family = fullPath[fullPath.length - 5];
+    if (fullPath[fullPath.length - 4] === "AVATAR") {
+      const animal = fullPath[fullPath.length - 3];
+      const type = fullPath[fullPath.length - 2];
+      const name = fullPath[fullPath.length - 1];
+      if (!svg[family]) {
+        svg[family] = {};
+      }
+      if (!svg[family][animal]) {
+        svg[family][animal] = {};
+      }
+      if (!svg[family][animal][type]) {
+        svg[family][animal][type] = {};
+      }
+      const content = await readFile(f, { encoding: "utf8" });
+      const x = content.match(/<svg .*>([\s\S]*)<\/svg>/m);
+      if (x) {
+        svg[family][animal][type][name.replace(".svg", "")] = x[1];
+      } else {
+        console.log("FILE COULD NOT BE PROCESSED!");
+        console.log(f);
+      }
     }
-    const content = await readFile(f, { encoding: "utf8" });
-    const x = content.match(/<svg .*>([\s\S]*)<\/svg>/m)
-    svgs[type].push(x[1]);
   }
 
   await rm("out", { recursive: true, force: true });
   await mkdir("out");
 
+  const max = 6000;
   let index = 1;
-  await print(Object.values(svgs), async (combo) => {
-    console.log('Start', index)
-    await writeFile(`out/${index}.svg`, `<svg xmlns="http://www.w3.org/2000/svg"  xmlns:xlink="http://www.w3.org/1999/xlink"  viewBox="0 0 432 432">
-    ${combo.join("\n")}</svg>`);
-    console.log(combo);
-    console.log("___");
-    index = index + 1;
-  });
+  while (index <= max) {
+    const families = Object.keys(svg);
+    const family = families[Math.floor(Math.random() * families.length)];
+    const animals = Object.keys(svg[family]);
+    const animal = animals[Math.floor(Math.random() * animals.length)];
+    const bodies = Object.keys(svg[family][animal].BODY);
+    const body = bodies[Math.floor(Math.random() * bodies.length)];
+    const bodyContent = svg[family][animal].BODY[body];
+    const outfits = Object.keys(svg[family][animal].OUTFIT);
+    const outfit = outfits[Math.floor(Math.random() * outfits.length)];
+    const outfitContent = svg[family][animal].OUTFIT[outfit];
+    const jewelries = Object.keys(svg[family][animal].JEWELRY);
+    const jewelry = jewelries[Math.floor(Math.random() * jewelries.length)];
+    const jewelryContent = svg[family][animal].JEWELRY[jewelry];
+
+    const filname = [
+      index,
+      family,
+      animal,
+      body.replace(animal, ""),
+      outfit.replace(animal, ""),
+      jewelry.replace(animal, ""),
+    ].join("-");
+
+    await writeFile(
+      `out/${filname}.svg`,
+      `<svg xmlns="http://www.w3.org/2000/svg"  xmlns:xlink="http://www.w3.org/1999/xlink"  viewBox="0 0 432 432">
+       ${bodyContent}\n
+       ${outfitContent}\n
+       ${jewelryContent}\n
+       </svg>`
+    );
+
+    index += 1;
+  }
 }
 
 main()
